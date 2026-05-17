a509a5f2b535b260212c13c53f2149895a224f27
Helder Barreto <helderbarreto23@gmail.com>
2026-05-17 19:06:43 -0300

    Comit Inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import SearchBar from "./components/SearchBar"
+
+
+function App() {
+
+  return (
+    // <div className="bg-blue-600 text-white">
+    //   tailwind funcionando 
+    // </div>
+
+    <SearchBar/>
+  )
+}
+
+export default App
