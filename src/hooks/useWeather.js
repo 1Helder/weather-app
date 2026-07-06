072977d2b727753c291e71c4f5b49bc44677daf3
Helder Barreto <helderbarreto23@gmail.com>
2026-07-05 22:11:50 -0300

    Trabalhando com a API

---
+++ src/hooks/useWeather.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { getCoordinates, getWeather } from "../services/weatherService";
+
+export function useWeather() {
+  const [weather, setWeather] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function searchCity(city) {
+    try {
+      setLoading(true);
+      setError(null);
+
+      const coordinates = await getCoordinates(city);
+
+      const weatherData = await getWeather(
+        coordinates.latitude,
+        coordinates.longitude,
+      );
+
+      setLocation(coordinates);
+      setWeather(weatherData);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return {
+    weather,
+    location,
+    loading,
+    error,
+    searchCity,
+  };
+}
